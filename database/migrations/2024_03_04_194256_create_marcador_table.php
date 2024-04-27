482a3af3e51ef863e287d730d3abcfa07f0e3837
Cledivaldo Campos <cledivaldojoseegg@gmail.com>
2024-04-27 18:16:15 -0300

    edt3

--- database/migrations/2024_03_04_194256_create_marcador_table.php
+++ database/migrations/2024_03_04_194256_create_marcador_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
+
+
 return new class extends Migration
 {
     /**
@@ -17,7 +20,7 @@ return new class extends Migration
             $table->string('minimo');
             $table->string('maximo');
             $table->string('masculino');
-            $table->string('Feminino');
+            $table->string('feminino');
             $table->timestamps();
         });
     }
